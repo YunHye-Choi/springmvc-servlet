2d885b81c81f80bb9ffadd6fbdc91b220eee2b46
yunhye-choi <yunhye_choi@tmax.co.kr>
2023-11-24 18:48:54 +0900

    [chore] 잘못 override한 메소드 수정 (public -> protected)

--- src/main/java/hello/servlet/basic/HelloServlet.java
+++ src/main/java/hello/servlet/basic/HelloServlet.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 @WebServlet(name = "helloServlet", urlPatterns = "/hello")
 public class HelloServlet extends HttpServlet {
     @Override
-    public void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         System.out.println("HelloServlet.service");
         System.out.println("request = " + request);
         System.out.println("response = " + response);
